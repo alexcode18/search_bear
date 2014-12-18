78f318fd41bea3d24c82d2d9662821751d98e2f6
alexcode18 <aschattner@gmail.com>
2014-12-18 16:31:23 -0500

    removed search controller and model

--- app/controllers/searches_controller.rb
+++
@@ -1,1 +0,0 @@
-searches_controller.rb
--- app/models/search.rb
+++
@@ -1,5 +0,0 @@
-class Search < ActiveRecord::Base
-	belongs_to :user
-	has_many :memories,  dependent: :destroy
-	validates_presence_of :user_id, :keyword
-end
