88908d9c87e8f35ceba26720029072094a01f3df
alexcode18 <aschattner@gmail.com>
2014-12-25 23:38:57 -0500

    the sign up is now working.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,26 @@ class UsersController < ApplicationController
 		render json: @user
 	end
 
+	def create
+    @user = User.new()
+		@user.parent_email = params[:parent_email]
+		@user.password = params[:password]
+		@user.password_confirmation = params[:password_confirmation]
+		@user.child_name = params[:child_name]
+		@user.child_gender = params[:child_gender]
+		@user.favorite_color = params[:favorite_color]
+
+    if @user.save
+      session[:current_user] = @user.id
+      render json: @user
+    else
+    	render json: @user.errors
+    end
+  end
+
 	private
 
 	def user_params
-		params.require(:user).permit(:parent_email, :password, :child_name, :favorite_color)
+		params.require(:user).permit(:parent_email, :password, :password_confirmation, :child_name, :child_gender, :favorite_color)
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	has_many :bears,  dependent: :destroy
 	has_many :searches, dependent: :destroy
 	
-	validates_presence_of :parent_email, :child_name, :favorite_color 
+	validates_presence_of :parent_email, :child_name, :favorite_color , :child_gender
 	validates_uniqueness_of :parent_email
 	has_secure_password
 end
